8d9c27ffc05b6f6cf7d31aa4fd8109b75ac8c077
Jorge Manrubia <jorge@37signals.com>
2024-11-26 00:19:06 +0100

    Remove redundant local vars

--- lib/pulse_wire/action_cable/persistent_cable_middleware.rb
+++ lib/pulse_wire/action_cable/persistent_cable_middleware.rb
@@ -6,12 +6,10 @@ class PulseWire::ActionCable::PersistentCableMiddleware
   def call(env)
     request = Rack::Request.new(env)
 
-    status, headers, response = if request.params["pulse_wire"]
+    if request.params["pulse_wire"]
       ActionCable.server.without_restarting { @app.call(env) }
     else
       @app.call(env)
     end
-
-    [ status, headers, response ]
   end
 end
