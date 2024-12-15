bb522279d3a2c62664a128b6baa3efc62b96f5de
Jorge Manrubia <jorge@37signals.com>
2024-12-15 16:15:33 +0100

    Move initialization to the engine so that customizations are picked

--- lib/hotwire_spark.rb
+++ lib/hotwire_spark.rb
@@ -6,9 +6,9 @@ loader = Zeitwerk::Loader.for_gem
 loader.setup
 
 module HotwireSpark
-  mattr_accessor :css_paths, default: %w[ app/assets/stylesheets ]
-  mattr_accessor :html_paths, default: %w[ app/controllers app/helpers app/models app/views ]
-  mattr_accessor :stimulus_paths, default: %w[ app/javascript/controllers ]
+  mattr_accessor :css_paths, default: []
+  mattr_accessor :html_paths, default: []
+  mattr_accessor :stimulus_paths, default: []
 
   mattr_accessor :enabled, default: Rails.env.development?
 
--- lib/hotwire_spark/engine.rb
+++ lib/hotwire_spark/engine.rb
@@ -5,6 +5,11 @@ module HotwireSpark
     isolate_namespace HotwireSpark
 
     config.hotwire_spark = ActiveSupport::OrderedOptions.new
+    config.hotwire_spark.merge! \
+      enabled: Rails.env.development?,
+      css_paths: %w[ app/assets/stylesheets ],
+      html_paths: %w[ app/controllers app/helpers app/models app/views ],
+      stimulus_paths: %w[ app/javascript/controllers ]
 
     initializer "hotwire_spark.config" do |app|
       config.hotwire_spark.each do |key, value|
